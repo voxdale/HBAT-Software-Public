65161e9ad75372da5fbb219abd1154be4cd2503b
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2021-10-24 13:30:31 +0200

    modified web_socket server
    added debug code and cpu core temp reading

--- HMSmain-Asyncwebserver/src/Websocket_Server.cpp
+++ HMSmain-Asyncwebserver/src/Websocket_Server.cpp
@@ -8,6 +8,14 @@
 #else
 #include <ESP8266WiFi.h>
 #endif
+
+#define DEBUGMODE 1
+#if DEBUGMODE == 1
+#define debugserial Serial1
+#else
+#define debugserial(x)
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -16,19 +24,6 @@ uint8_t temprature_sens_read();
 
 #ifdef __cplusplus
 }
-#endif
-
-#define DEBUG 0
-
-#if DEBUG == 1
-#define debug(x) Serial.print(x)
-#define debugln(x) Serial.println(x)
-#define debugf(x) Serial.printf(x)
-
-#else
-#define debug(x)
-#define debugln(x)
-#define debugf(x)
 #endif
 
 uint8_t temprature_sens_read();
@@ -41,19 +36,19 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
 
   switch (type) {
     case WStype_DISCONNECTED:
-      debug.printf("[%u] Disconnected!\n", num);
+      debugserial.printf("[%u] Disconnected!\n", num);
       break;
     case WStype_CONNECTED:
       {
         IPAddress ip = webSocket.remoteIP(num);
-        debug.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+        debugserial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
 
         // send message to client
         webSocket.sendTXT(num, "Connected");
       }
       break;
     case WStype_TEXT:
-      debug.printf("[%u] get Text: %s\n", num, payload);
+      debugserial.printf("[%u] get Text: %s\n", num, payload);
 
       // send message to client
       webSocket.sendTXT(num, "This is a test of sample text");
@@ -62,7 +57,7 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
       // webSocket.broadcastTXT("message here");
       break;
     case WStype_BIN:
-      debug.printf("[%u] get binary length: %u\n", num, length);
+      debugserial.printf("[%u] get binary length: %u\n", num, length);
       //hexdump(payload, length);
 
       // send message to client
@@ -74,24 +69,24 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
 void setup()
 {
   Serial.begin(115200);
-  debug.println();
+  debugserial.println();
 
   WiFi.begin(ssid, password);
 
-  debug.print("Connecting");
+  debugserial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    debug.print(".");
+    debugserial.print(".");
   }
-  debug.println();
+  debugserial.println();
 
-  debug.print("Connected, IP address: ");
-  debug.println(WiFi.localIP());
+  debugserial.print("Connected, IP address: ");
+  debugserial.println(WiFi.localIP());
 
-  debug.println('\n');
-  debug.println("Connection established!");
-  debug.print("IP address:\t");
-  debug.println(WiFi.localIP());                   // Send the IP address of the ESP to the computer
+  debugserial.println('\n');
+  debugserial.println("Connection established!");
+  debugserial.print("IP address:\t");
+  debugserial.println(WiFi.localIP());                   // Send the IP address of the ESP to the computer
 
   // port and URL
   webSocket.begin();
@@ -100,11 +95,11 @@ void setup()
 }
 
 void loop() {
-  debug.print("Temperature: ");
+  debugserial.print("Temperature: ");
 
   // Convert raw core temperature in F to Celsius degrees
-  debug.print((temprature_sens_read() - 32) / 1.8);
-  debug.println(" C");
+  debugserial.print((temprature_sens_read() - 32) / 1.8);
+  debugserial.println(" C");
   delay(1000);
   webSocket.loop();
 }
